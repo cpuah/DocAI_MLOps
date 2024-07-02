4cd868b54129f9bba7cdb48a1838faa25f600ede
Calvin Puah <calvinpuah@gmail.com>
2024-07-02 14:48:48 +0930

    Edit

--- infra/Deploy-Infrastructure.ps1
+++ infra/Deploy-Infrastructure.ps1
@@ -28,13 +28,15 @@ Push-Location -Path $PSScriptRoot
 
 az --version
 
-$DeploymentOutputs = (az deployment sub create --name $DeploymentName --location $Location --template-file './main.bicep' `
-        --parameters './main.parameters.json' `
-        --parameters workloadName=$DeploymentName `
-        --parameters location=$Location `
-        --query 'properties.outputs' -o json) | ConvertFrom-Json
-$DeploymentOutputs | ConvertTo-Json | Out-File -FilePath './InfrastructureOutputs.json' -Encoding utf8
+
+
+#$DeploymentOutputs = (az deployment sub create --name $DeploymentName --location $Location --template-file './main.bicep' `
+#        --parameters './main.parameters.json' `
+#        --parameters workloadName=$DeploymentName `
+#        --parameters location=$Location `
+#        --query 'properties.outputs' -o json) | ConvertFrom-Json
+#$DeploymentOutputs | ConvertTo-Json | Out-File -FilePath './InfrastructureOutputs.json' -Encoding utf8
 
 Pop-Location
 
-return $DeploymentOutputs
+#return $DeploymentOutputs
