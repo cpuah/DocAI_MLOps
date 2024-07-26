50480a5bd1ad7e024030178de5c059f98ff78f49
Calvin Puah <calvinpuah@gmail.com>
2024-07-26 14:05:08 +0930

    sync

---
+++ sample_analyze_custom_documents.py
@@ -0,0 +1,82 @@
+"""
+This code sample shows Custom Extraction Model operations with the Azure Form Recognizer client library. 
+The async versions of the samples require Python 3.6 or later.
+
+To learn more, please visit the documentation - Quickstart: Form Recognizer Python client library SDKs
+https://learn.microsoft.com/azure/applied-ai-services/form-recognizer/quickstarts/get-started-v3-sdk-rest-api?view=doc-intel-3.1.0&pivots=programming-language-python
+"""
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+"""
+Remember to remove the key from your code when you're done, and never post it publicly. For production, use
+secure methods to store and access your credentials. For more information, see 
+https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
+"""
+endpoint = "https://di-y7us5axonhst4.cognitiveservices.azure.com/"
+key = "41b3897811734dc983407b2971811f61"
+
+# The name of the PDF file the user is providing.
+working_dir = os.path.abspath('')
+pdf_file_name = 'Invoice_6.pdf'
+#pdf_file_name = 'invoice_AG42923.pdf'
+
+base_file_name = pdf_file_name[:-4]  # Slice the string, excluding the last 4 characters ('.pdf')
+
+# The directory containing the PDF file.
+pdf_dir = os.path.join(working_dir, 'pdfs')
+
+# The file path to the PDF file for loading.
+pdf_path = os.path.join(pdf_dir, pdf_file_name)
+
+model_id = "invoices-1.1.0"
+formUrl = os.path.join(pdf_dir, pdf_file_name)
+
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+# Make sure your document's type is included in the list of document types the custom model can analyze
+poller = document_analysis_client.begin_analyze_document_from_url(model_id, formUrl)
+result = poller.result()
+
+for idx, document in enumerate(result.documents):
+    print("--------Analyzing document #{}--------".format(idx + 1))
+    print("Document has type {}".format(document.doc_type))
+    print("Document has confidence {}".format(document.confidence))
+    print("Document was analyzed by model with ID {}".format(result.model_id))
+    for name, field in document.fields.items():
+        field_value = field.value if field.value else field.content
+        print("......found field of type '{}' with value '{}' and with confidence {}".format(field.value_type, field_value, field.confidence))
+
+
+# iterate over tables, lines, and selection marks on each page
+for page in result.pages:
+    print("\nLines found on page {}".format(page.page_number))
+    for line in page.lines:
+        print("...Line '{}'".format(line.content.encode('utf-8')))
+    for word in page.words:
+        print(
+            "...Word '{}' has a confidence of {}".format(
+                word.content.encode('utf-8'), word.confidence
+            )
+        )
+    for selection_mark in page.selection_marks:
+        print(
+            "...Selection mark is '{}' and has a confidence of {}".format(
+                selection_mark.state, selection_mark.confidence
+            )
+        )
+
+for i, table in enumerate(result.tables):
+    print("\nTable {} can be found on page:".format(i + 1))
+    for region in table.bounding_regions:
+        print("...{}".format(i + 1, region.page_number))
+    for cell in table.cells:
+        print(
+            "...Cell[{}][{}] has content '{}'".format(
+                cell.row_index, cell.column_index, cell.content.encode('utf-8')
+            )
+        )
+print("-----------------------------------")
